50b5f0adda12118f154fb2983159cf3480eae5c9
CallumDotJ <162898463+CallumDotJ@users.noreply.github.com>
2026-03-26 16:34:53 +0000

    serverfix

--- submit-app/swagger.js
+++ submit-app/swagger.js
@@ -7,6 +7,11 @@ const options = {
       title: "Submit API",
       version: "1.0.0",
     },
+    servers: [
+      {
+        url: "https://158.158.35.176/submit-api",
+      },
+    ],
   },
   apis: ["./routes/*.js", "server.js"], // Has to be array
 };
